f9ffbde89b163f2d8eefde75c386f967d6aa5feb
Beata77 <2007.beata@gmail.com>
2024-02-15 16:12:47 +0000

    initial commit

---
+++ clone1.js
@@ -0,0 +1,12 @@
+
+
+$("#menuButton").click(function(){
+    $("#exit").toggle();
+    $("#burger").toggle();
+    
+  });
+
+  
+$('#menuButton').click(function(){
+    $('.detailedMenu').toggle('slow', 'linear');
+    });
